1d971506ed158baf6f6f057f9556d8bba94bcd8c
nguyennam <nguyennam150796@gmail.com>
2024-01-24 16:42:28 +0700

    fix mysql host to connection

--- nodejs/app.js
+++ nodejs/app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
-  host: "db",
+  host: "mysql",
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: "users",
