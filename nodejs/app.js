89bc12e7ca487bddc278b73635a0f21b7262d93b
nguyennam <nguyennam150796@gmail.com>
2024-01-25 22:54:49 +0700

    add mysql env

--- nodejs/app.js
+++ nodejs/app.js
@@ -14,7 +14,7 @@ const pool = mysql.createPool({
   host: "mysql",
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: "users",
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
